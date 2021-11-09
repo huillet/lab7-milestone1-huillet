da0a10bef319e92a5cb0eacea78975686a5d5901
Levi Huillet <levimhuillet@gmail.com>
2021-11-08 19:56:33 -0600

    can send message on channel 1

--- app/src/main/java/com/example/lab7_milestone1_huillet/MainActivity.java
+++ app/src/main/java/com/example/lab7_milestone1_huillet/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.lab7_milestone1_huillet;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.lab7_milestone1_huillet.App.CHANNEL_1_ID;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+        editTextTitle = findViewById(R.id.titleText);
+        editTextMessage = findViewById(R.id.messageText);
     }
+
+    public void sendOnChannel(View view)
+    {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_baseline_chat_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
 }
